f551d0761b1c6018e45538ce38101fe4bf7c38e3
spacekomad <spacekomad@gmail.com>
2023-03-30 16:00:56 +0900

    roll 버그 수정

--- Assets/Lab/Script/PlayerStateMachine/PlayerRollingState.cs
+++ Assets/Lab/Script/PlayerStateMachine/PlayerRollingState.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d45d076140355e602e5da5ab1430b2a9634325ec5db97f80928a52daa2dc4a2b
-size 2662
+oid sha256:c8de354013e1545cb541b4d920003bf295974506040f57685be17c6543ef8d4c
+size 2705
