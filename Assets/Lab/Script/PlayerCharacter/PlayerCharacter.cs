a01d07161b62f51effacbf2e2324994a82826bbe
spacekomad <spacekomad@gmail.com>
2023-04-17 11:59:51 +0900

    Player Gravity Setting & Falling Motion Setting

--- Assets/Lab/Script/PlayerCharacter/PlayerCharacter.cs
+++ Assets/Lab/Script/PlayerCharacter/PlayerCharacter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69bba5c69eee54faef2e8803ae7d81155a6d12899469da26c0977367fb15212e
-size 12745
+oid sha256:617cc24257b3ec96cb16491bf0628802f6d3faaebacf114451d109e222a3bf1f
+size 12885
