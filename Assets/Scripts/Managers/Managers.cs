77808b63b4466fbd6aafe2396499c316756e77e0
pollux-13 <strakk@naver.com>
2023-04-19 12:56:13 +0900

    UI work_0418

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2b53599022e928105c90e25c26186c386efa09748ef3c9d3dcf8cea5eb11f8a
+size 767
---
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:591b7efb38b8234f986a82b9bfcebd3330cd1bd67c07d40cd7aea4b80b370318
+size 706
